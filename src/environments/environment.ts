83483ad099ee14b1ccb60d6b7bca75c7027b136c
Zain Ul Abdeen <zu4425@gmail.com>
2025-10-02 14:20:59 +0500

    Envoriment updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://cui-internship-git-dev-talhas-projects-59c8907e.vercel.app',
-  recaptchaSiteKey: 'YOUR_RECAPTCHA_SITE_KEY' // TODO: replace with real production site key
+ 
 };
